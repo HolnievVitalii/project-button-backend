b047115a39277ac0ba609cfe4c8d07c784dc84d9
Sleeper32 <wosamrub@gmail.com>
2019-12-11 21:19:47 +1000

    generate initializator

---
+++ button/src/main/java/com/button/ButtonApplication.java
@@ -0,0 +1,13 @@
+package com.button;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButtonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ButtonApplication.class, args);
+	}
+
+}
---
+++ button/src/test/java/com/button/ButtonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.button;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButtonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
