d7845ed11f00b67398e920372768cf0f68aeeefa
max <mgburmasov@koil.ru>
2019-12-13 15:43:59 +1000

    rendering list on main page

--- button/src/main/java/com/button/web/controller/IndexController.java
+++ button/src/main/java/com/button/web/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.button.web.controller;
 import com.button.model.Product;
 import com.button.model.ProductList;
 import com.button.model.ProductListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,28 +11,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping(value = {"/", "index"})
 public class IndexController {
+    @Autowired
     ProductListRepository productListRepository;
-
-    class ListRecord {
-        public Integer id;
-        public String name;
-    }
 
     @GetMapping
     public String index(Model model) {
-        List<ProductList> productLists = (List<ProductList>) productListRepository.findAll();
-        List<ListRecord> lists= new ArrayList<>();
+        Iterable<ProductList> productLists;
 
-        for (ProductList pl : productLists) {
-            ListRecord lr = new ListRecord();
-            lr.id = pl.getId();
-            lr.name = pl.getName();
-        }
-        model.addAttribute("lists", lists);
+        productLists = productListRepository.findAll();
+        model.addAttribute("productLists", productLists);
 
         return "index";
     }
