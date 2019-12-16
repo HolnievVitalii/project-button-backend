c1df42bf12d55fb99093f684ffba1bfddcac35ae
max <mgburmasov@koil.ru>
2019-12-16 14:04:06 +1000

    implemented deleting product_list item

--- button/src/main/java/com/button/web/controller/ProductListPageController.java
+++ button/src/main/java/com/button/web/controller/ProductListPageController.java
@@ -1,16 +1,28 @@
 package com.button.web.controller;
 
+import com.button.model.ProductList;
+import com.button.model.ProductListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
-@RequestMapping("/new_product_list")
+@RequestMapping("/product_list")
 public class ProductListPageController {
-    @GetMapping
-    public String newProductListPage() {
+    @Autowired
+    ProductListRepository productListRepository;
+
+    @GetMapping("/new")
+    public String addNewProductListPage() {
         return "new_product_list";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteProductListPage(@PathVariable Integer id) {
+        productListRepository.deleteById(id);
+        return "redirect:/index";
     }
 }
