6e7bd799be33cf6f73d514e8f2ffa536a379d35e
Sleeper32 <wosamrub@gmail.com>
2019-12-16 23:27:53 +1000

    creating query for getting product properties by product_list_id

--- button/src/main/java/com/button/model/ProductPropertyRepository.java
+++ button/src/main/java/com/button/model/ProductPropertyRepository.java
@@ -1,6 +1,21 @@
 package com.button.model;
 
+import org.hibernate.validator.constraints.ParameterScriptAssert;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProductPropertyRepository extends CrudRepository<ProductProperty, Integer> {
+//    @Query("SELECT pp\n" +
+//            "FROM product_property pp\n" +
+//            "INNER JOIN product p ON\n" +
+//            "\tpp.product_id = p.id\n" +
+//            "INNER JOIN product_list pl ON\n" +
+//            "\tpp.product_list_id = pl.id\n" +
+//            "WHERE\n" +
+//            "\tpl.id = :list_id")
+    @Query("SELECT pp FROM product_property pp WHERE pp.product_list_id = :list_id")
+    List<ProductProperty> findProductsByProductListId(@Param("list_id") Integer listId);
 }
