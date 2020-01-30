7643a140762ad2c16d0bf80876efa9b8ce9d7492
max <mgburmasov@koil.ru>
2020-01-30 16:40:10 +1000

    fix for deploying

--- button/src/main/java/com/button/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.button;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer
-        extends AbstractSecurityWebApplicationInitializer {
-}
